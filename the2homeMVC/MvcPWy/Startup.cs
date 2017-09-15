7fdd5c0d31bb0194fdf9633be31cea06c59c91ea
Frrrrr <cnyangdengau@gmail.com>
2017-09-16 02:20:43 +1000

    TheWeek8Friday
    Register with Email Authentication

---
+++ the2homeMVC/MvcPWy/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using MvcPWy;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace MvcPWy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
